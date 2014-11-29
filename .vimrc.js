9030ad3cffd1b1a9400380f54eca2283fb14c0a6
hachi-eiji <hachiyae@gmail.com>
2014-11-29 23:41:33 +0900

    show quick fix error if an error occurred

--- .vimrc.js
+++ .vimrc.js
@@ -6,6 +6,9 @@ Bundle 'guileen/vim-node-dict'
 Bundle 'maksimr/vim-jsbeautify'
 
 let g:syntastic_javascript_checkers = ['jshint']
+let g:syntastic_enable_signs= 1
+let g:syntastic_auto_loc_list= 1
+
 let g:jscomplete_use = ['dom', 'moz']
 au FileType javascript set dictionary+=$HOME/.vim/dict/node.dict
 autocmd FileType javascript
